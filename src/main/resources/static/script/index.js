a9d85253e15f2f6d6599a1712bb40d2104eb46d2
TreleaniFederico <treleani.federico@gmail.com>
2023-03-06 13:32:06 +0100

    add front end tag/title search by

--- src/main/resources/static/script/index.js
+++ src/main/resources/static/script/index.js
@@ -12,11 +12,11 @@ function photoList() {
             response.data.forEach(photo => {
                 document.querySelector('#photo_table').innerHTML += `
 
-                <tr>
-                    <td>${photo.title}</td>
+                <tr class="photo">
+                    <td class="photo-title">${photo.title}</td>
                     <td>${photo.description}</td>
                     <td><img class="img-thumbnail" width="150" height="150" src="${photo.url}"></img></td>
-                    <td>${photo.tag}</td>
+                    <td class="photo-tag">${photo.tag}</td>
                     
                     <td><a class="btn btn-primary" href="./my-photos/show?id=${photo.id}"><i class="fa-solid fa-magnifying-glass"></i></a></td>
                 </tr>
@@ -27,4 +27,22 @@ function photoList() {
         .catch((response) => {
             console.error('Errore chiamata', response);
         })
+}
+
+function search() {
+	const search = document.querySelector('#search');
+	const value = search.value.toLowerCase();
+	console.log(value);
+	const photos = document.querySelectorAll('.photo');
+	photos.forEach((photo) => {
+		console.log(photo);
+		const name = photo.querySelector('.photo-title').textContent.toLowerCase();
+		const tag = photo.querySelector('.photo-tag').textContent.toLowerCase();
+		console.log(name);
+		if(name.includes(value) || tag.includes(value)){
+			photo.classList.remove('d-none');
+		} else {
+			photo.classList.add('d-none');
+		}
+	})
 }
